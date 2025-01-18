cc940b7ca8fa3c598fc4601002d1237846ce868f
nether1203 <162194192+nether1203@users.noreply.github.com>
2025-01-18 18:05:33 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -3,8 +3,8 @@ const container = document.querySelector('.cardsbox')
 
 
 
-function getCharectrrs () {
-    fetch(baseUrl)
+function getCharectrrs (page, gender, status) {
+    fetch(`${baseUrl}/?page=${page}&status=${status}&gender=${gender}`)
     .then(response => response.json())
     .then(data =>{ 
         renderCards(data.results)
@@ -12,7 +12,7 @@ function getCharectrrs () {
     })
 }
 
-getCharectrrs();
+getCharectrrs(1,'', '');
 
 function renderCards (data) {
     container.innerHTML = '';
@@ -23,34 +23,84 @@ function renderCards (data) {
           <div class="card-body">
             <h5 class="card-title">${cardData.name}</h5>
             <p class="card-text">${cardData.species}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <p class="card-text" >${cardData.status}</p>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="${cardData.id}">Deteils</button>
           </div>
           </div>`
         
     });
 }
 
-
+let currentPage = 1;
 function renderPagination(info){
-    const paginationbox = document.querySelector('.paginationbox')
-    paginationbox.innerHTML = `
-<nav aria-label="Page navigation example">
-  <ul class="pagination">
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous">
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
-  </ul>
-</nav>`
+  
+    const paginationbox = document.querySelector('.pagination')
+  paginationbox.innerHTML = ``;
+  paginationbox.innerHTML +=
+     `<li class="page-item prevPage ">
+            <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+             </a>
+            </li>`;
+            paginationbox.innerHTML+=`<li class="page-item"><a class="page-link" href="#">${currentPage}</a></li>
+            ` ;
+  paginationbox.innerHTML+=`
+            <li class="page-item nextPage">
+              <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>`;
+ 
+  
 
+  const nextPage = document.querySelector('.nextPage')
+  const prevPage = document.querySelector('.prevPage')
+  nextPage.addEventListener('click', () => {
+    currentPage++;
+
+    console.log('next page');
+
+    getCharectrrs(currentPage, '' , '');
+    
+  })
+  prevPage.addEventListener('click', () => {
+    console.log('prev page' );
+    if (currentPage > 1){
+      currentPage--;
+      getCharectrrs(currentPage, '', '');
     }
+    
 
+    
+  })
+  }
+
+const btnfilter = document.querySelector('.btnFilter');
+
+btnfilter.addEventListener('click', () => {
+  const gender = document.querySelector('#SelectGender').value;
+  const status = document.querySelector('#SelectStatus').value;
+  currentPage = 1;
+  console.log(gender,status);
+  if(gender === ''){
+    getCharectrrs(currentPage,'',status)
+  }else if (status === ''){
+    getCharectrrs(currentPage, gender, '')
+  }else{
+    getCharectrrs(currentPage, gender , status)
+  };
+  // getCharectrrs(gender,status)
+})
+
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    const button = event.relatedTarget 
+    const id = button.getAttribute('data-bs-whatever')
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    fetch(`${baseUrl}/${id}`)
+    
+    modalTitle.textContent = `Info about ${id}`
+    
+  })
+}
